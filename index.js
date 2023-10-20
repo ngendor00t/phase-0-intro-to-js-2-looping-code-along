7a27597a0825899d2f6fefaa326ce54c56b3359b
Reginawambui <ngendoanita8@gmail.com>
2023-10-20 09:19:21 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let mySpy = [];
+let Spy = ["Harry", "James", "Potter"];
+function writeCards(spyname, eventname){
+    for(let count = 0; count < spyname.length; count++){
+        mySpy.push(`Thank you, ${spyname[count]}, for the wonderful ${eventname} gift!`);
+    }
+    return mySpy;
+
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
+countDown(10);
+writeCards(spy, "surprise");
